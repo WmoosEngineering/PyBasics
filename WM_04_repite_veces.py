e08c0d72b2822142e1e524b12737b57118650519
WmoosEngineering <56775748+WmoosEngineering@users.noreply.github.com>
2019-10-25 18:50:16 -0500

    Add files via upload

---
+++ WM_04_repite_veces.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Oct 25 18:48:23 2019
+
+@author: Wmoos
+"""
+
+veces=int(input("Cuantas veces quieres que te salude"))
+for i in range(veces):
+    print("Hola")
+print()
+print("Adios")    
