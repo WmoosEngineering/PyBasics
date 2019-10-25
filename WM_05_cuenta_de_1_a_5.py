0d48d89059d49a96a78e717c6cfdc2657765b1a5
WmoosEngineering <56775748+WmoosEngineering@users.noreply.github.com>
2019-10-25 18:48:14 -0500

    Add files via upload

---
+++ WM_05_cuenta_de_1_a_5.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Oct 25 18:45:36 2019
+
+@author: Wmoos
+"""
+
+print("Comienzo")
+cuenta=0
+for i in range(1,6):
+    if i % 2 == 0:
+        cuenta=cuenta+1
+print("Desde 1 hasta 5 hay",cuenta,"multiplos de 2")
