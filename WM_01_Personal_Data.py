86af48fc8b96437c553a7f3ee69eba23f080b8c9
WmoosEngineering <56775748+WmoosEngineering@users.noreply.github.com>
2019-10-25 13:58:17 -0500

    Add files via upload

---
+++ WM_01_Personal_Data.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Oct 19 12:04:06 2019
+
+@author: Wmoos
+"""
+
+fn=input("Cual es tu nombre: ")
+ln=input("Cual es tu apellido: ")
+loc=input("Cual es tu ubicacion: ")
+age=input("Cual es tu edad: ")
+space=""
+print("\n")
+str1="Hola Bienvenido"
+str2="Tu"
+str3="es:"
+print(str1,fn,"!")
+print(str2,"apellido",str3,ln)
+print(str2,"ubicacion",str3,loc)
+print(str2,"edad",str3,age)
+
