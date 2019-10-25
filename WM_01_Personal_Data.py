d118149ce5ff1f4ff2acb3336d3f7bd69a3a4423
WmoosEngineering <56775748+WmoosEngineering@users.noreply.github.com>
2019-10-25 18:45:05 -0500

    Add files via upload

--- WM_01_Personal_Data.py
+++ WM_01_Personal_Data.py
@@ -8,7 +8,13 @@ Created on Sat Oct 19 12:04:06 2019
 fn=input("Cual es tu nombre: ")
 ln=input("Cual es tu apellido: ")
 loc=input("Cual es tu ubicacion: ")
-age=input("Cual es tu edad: ")
+age=int(input("Cual es tu edad: "))
+
+if age>=18:
+    str4="Mayor de edad"
+else:
+    str4="Menor de edad"
+    
 space=""
 print("\n")
 str1="Hola Bienvenido"
@@ -18,4 +24,5 @@ print(str1,fn,"!")
 print(str2,"apellido",str3,ln)
 print(str2,"ubicacion",str3,loc)
 print(str2,"edad",str3,age)
+print(str4)
 
